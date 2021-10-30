c4f6afe19834cbef8144a408cabf90eef4da30c5
STP-PMT <62011212109@msu.ac.th>
2021-10-31 01:10:16 +0700

    add ui detail orders done

--- AppBooks/Page/FormOrders.Designer.cs
+++ AppBooks/Page/FormOrders.Designer.cs
@@ -43,6 +43,15 @@ namespace AppBooks
             this.labelName = new System.Windows.Forms.Label();
             this.labelDetailBook = new System.Windows.Forms.Label();
             this.lbName = new System.Windows.Forms.Label();
+            this.lbDetail = new System.Windows.Forms.Label();
+            this.labelSdate = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbSdate = new System.Windows.Forms.Label();
+            this.lbEdate = new System.Windows.Forms.Label();
+            this.labelUser = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbUserName = new System.Windows.Forms.Label();
+            this.lbPhone = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.tabAll.SuspendLayout();
@@ -59,7 +68,7 @@ namespace AppBooks
             this.dgvOrders.Location = new System.Drawing.Point(3, 3);
             this.dgvOrders.Name = "dgvOrders";
             this.dgvOrders.ReadOnly = true;
-            this.dgvOrders.Size = new System.Drawing.Size(546, 297);
+            this.dgvOrders.Size = new System.Drawing.Size(546, 373);
             this.dgvOrders.TabIndex = 0;
             // 
             // monthCalendar
@@ -80,7 +89,7 @@ namespace AppBooks
             this.tabControl1.Location = new System.Drawing.Point(12, 170);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(560, 329);
+            this.tabControl1.Size = new System.Drawing.Size(560, 405);
             this.tabControl1.TabIndex = 2;
             // 
             // tabAll
@@ -90,7 +99,7 @@ namespace AppBooks
             this.tabAll.Location = new System.Drawing.Point(4, 22);
             this.tabAll.Name = "tabAll";
             this.tabAll.Padding = new System.Windows.Forms.Padding(3);
-            this.tabAll.Size = new System.Drawing.Size(552, 303);
+            this.tabAll.Size = new System.Drawing.Size(552, 379);
             this.tabAll.TabIndex = 0;
             this.tabAll.Text = "ทั้งหมด";
             this.tabAll.UseVisualStyleBackColor = true;
@@ -131,13 +140,22 @@ namespace AppBooks
             this.panelDetailOrder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panelDetailOrder.BackColor = System.Drawing.Color.Gainsboro;
+            this.panelDetailOrder.Controls.Add(this.lbPhone);
+            this.panelDetailOrder.Controls.Add(this.lbUserName);
+            this.panelDetailOrder.Controls.Add(this.label6);
+            this.panelDetailOrder.Controls.Add(this.labelUser);
+            this.panelDetailOrder.Controls.Add(this.lbEdate);
+            this.panelDetailOrder.Controls.Add(this.lbSdate);
+            this.panelDetailOrder.Controls.Add(this.label2);
+            this.panelDetailOrder.Controls.Add(this.labelSdate);
+            this.panelDetailOrder.Controls.Add(this.lbDetail);
             this.panelDetailOrder.Controls.Add(this.lbName);
             this.panelDetailOrder.Controls.Add(this.labelDetailBook);
             this.panelDetailOrder.Controls.Add(this.labelName);
             this.panelDetailOrder.Controls.Add(this.pictureBox1);
             this.panelDetailOrder.Location = new System.Drawing.Point(584, 192);
             this.panelDetailOrder.Name = "panelDetailOrder";
-            this.panelDetailOrder.Size = new System.Drawing.Size(227, 300);
+            this.panelDetailOrder.Size = new System.Drawing.Size(227, 376);
             this.panelDetailOrder.TabIndex = 5;
             // 
             // labelTitleOrders
@@ -173,7 +191,7 @@ namespace AppBooks
             // 
             this.labelDetailBook.AutoSize = true;
             this.labelDetailBook.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelDetailBook.Location = new System.Drawing.Point(8, 142);
+            this.labelDetailBook.Location = new System.Drawing.Point(8, 152);
             this.labelDetailBook.Name = "labelDetailBook";
             this.labelDetailBook.Size = new System.Drawing.Size(78, 19);
             this.labelDetailBook.TabIndex = 2;
@@ -181,17 +199,103 @@ namespace AppBooks
             // 
             // lbName
             // 
+            this.lbName.BackColor = System.Drawing.Color.White;
             this.lbName.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbName.Location = new System.Drawing.Point(92, 123);
             this.lbName.Name = "lbName";
             this.lbName.Size = new System.Drawing.Size(120, 19);
             this.lbName.TabIndex = 3;
             // 
+            // lbDetail
+            // 
+            this.lbDetail.BackColor = System.Drawing.Color.White;
+            this.lbDetail.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDetail.Location = new System.Drawing.Point(92, 152);
+            this.lbDetail.Name = "lbDetail";
+            this.lbDetail.Size = new System.Drawing.Size(120, 52);
+            this.lbDetail.TabIndex = 4;
+            // 
+            // labelSdate
+            // 
+            this.labelSdate.AutoSize = true;
+            this.labelSdate.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSdate.Location = new System.Drawing.Point(30, 219);
+            this.labelSdate.Name = "labelSdate";
+            this.labelSdate.Size = new System.Drawing.Size(56, 19);
+            this.labelSdate.TabIndex = 5;
+            this.labelSdate.Text = "วันที่ยืม :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 247);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "กำหนดยืน :";
+            // 
+            // lbSdate
+            // 
+            this.lbSdate.BackColor = System.Drawing.Color.White;
+            this.lbSdate.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSdate.Location = new System.Drawing.Point(92, 219);
+            this.lbSdate.Name = "lbSdate";
+            this.lbSdate.Size = new System.Drawing.Size(120, 19);
+            this.lbSdate.TabIndex = 7;
+            // 
+            // lbEdate
+            // 
+            this.lbEdate.BackColor = System.Drawing.Color.White;
+            this.lbEdate.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEdate.Location = new System.Drawing.Point(92, 247);
+            this.lbEdate.Name = "lbEdate";
+            this.lbEdate.Size = new System.Drawing.Size(120, 19);
+            this.lbEdate.TabIndex = 8;
+            // 
+            // labelUser
+            // 
+            this.labelUser.AutoSize = true;
+            this.labelUser.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUser.Location = new System.Drawing.Point(43, 282);
+            this.labelUser.Name = "labelUser";
+            this.labelUser.Size = new System.Drawing.Size(41, 19);
+            this.labelUser.TabIndex = 9;
+            this.labelUser.Text = "ผู้ยืม :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(21, 310);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 19);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "เบอร์โทร :";
+            // 
+            // lbUserName
+            // 
+            this.lbUserName.BackColor = System.Drawing.Color.White;
+            this.lbUserName.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUserName.Location = new System.Drawing.Point(92, 282);
+            this.lbUserName.Name = "lbUserName";
+            this.lbUserName.Size = new System.Drawing.Size(120, 19);
+            this.lbUserName.TabIndex = 11;
+            // 
+            // lbPhone
+            // 
+            this.lbPhone.BackColor = System.Drawing.Color.White;
+            this.lbPhone.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPhone.Location = new System.Drawing.Point(92, 310);
+            this.lbPhone.Name = "lbPhone";
+            this.lbPhone.Size = new System.Drawing.Size(120, 19);
+            this.lbPhone.TabIndex = 12;
+            // 
             // FormOrders
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(829, 511);
+            this.ClientSize = new System.Drawing.Size(829, 587);
             this.Controls.Add(this.labelTitleOrders);
             this.Controls.Add(this.panelDetailOrder);
             this.Controls.Add(this.btnSearch);
@@ -227,5 +331,14 @@ namespace AppBooks
         private System.Windows.Forms.Label labelName;
         private System.Windows.Forms.Label labelDetailBook;
         private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Label lbEdate;
+        private System.Windows.Forms.Label lbSdate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelSdate;
+        private System.Windows.Forms.Label lbDetail;
+        private System.Windows.Forms.Label lbPhone;
+        private System.Windows.Forms.Label lbUserName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelUser;
     }
 }
