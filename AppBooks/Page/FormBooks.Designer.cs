6604f42bd4cd3f98d47be0072ad17ee8dd1e815d
STP-PMT <62011212109@msu.ac.th>
2021-10-31 15:11:45 +0700

    add Form manage books and orders

--- AppBooks/Page/FormBooks.Designer.cs
+++ AppBooks/Page/FormBooks.Designer.cs
@@ -29,12 +29,280 @@ namespace AppBooks
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbEdate = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.labelTitleOrders = new System.Windows.Forms.Label();
+            this.lbSdate = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelSdate = new System.Windows.Forms.Label();
+            this.lbDetail = new System.Windows.Forms.Label();
+            this.lbName = new System.Windows.Forms.Label();
+            this.labelDetailBook = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelDetailOrder = new System.Windows.Forms.Panel();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.tabAll = new System.Windows.Forms.TabPage();
+            this.dgvOrdersAll = new System.Windows.Forms.DataGridView();
+            this.btnAddBook = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelDetailOrder.SuspendLayout();
+            this.tabAll.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrdersAll)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbEdate
+            // 
+            this.lbEdate.BackColor = System.Drawing.Color.White;
+            this.lbEdate.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEdate.Location = new System.Drawing.Point(92, 247);
+            this.lbEdate.Name = "lbEdate";
+            this.lbEdate.Size = new System.Drawing.Size(120, 19);
+            this.lbEdate.TabIndex = 8;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(846, 143);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 17;
+            this.btnDelete.Text = "ลบ";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEdit.Location = new System.Drawing.Point(765, 143);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 16;
+            this.btnEdit.Text = "แก้ไข";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // labelTitleOrders
+            // 
+            this.labelTitleOrders.Font = new System.Drawing.Font("Prompt", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitleOrders.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(125)))), ((int)(((byte)(254)))));
+            this.labelTitleOrders.Location = new System.Drawing.Point(12, 24);
+            this.labelTitleOrders.Name = "labelTitleOrders";
+            this.labelTitleOrders.Size = new System.Drawing.Size(110, 45);
+            this.labelTitleOrders.TabIndex = 15;
+            this.labelTitleOrders.Text = "หนังสือ";
+            // 
+            // lbSdate
+            // 
+            this.lbSdate.BackColor = System.Drawing.Color.White;
+            this.lbSdate.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSdate.Location = new System.Drawing.Point(92, 219);
+            this.lbSdate.Name = "lbSdate";
+            this.lbSdate.Size = new System.Drawing.Size(120, 19);
+            this.lbSdate.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 247);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "กำหนดยืน :";
+            // 
+            // labelSdate
+            // 
+            this.labelSdate.AutoSize = true;
+            this.labelSdate.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSdate.Location = new System.Drawing.Point(30, 219);
+            this.labelSdate.Name = "labelSdate";
+            this.labelSdate.Size = new System.Drawing.Size(56, 19);
+            this.labelSdate.TabIndex = 5;
+            this.labelSdate.Text = "วันที่ยืม :";
+            // 
+            // lbDetail
+            // 
+            this.lbDetail.BackColor = System.Drawing.Color.White;
+            this.lbDetail.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDetail.Location = new System.Drawing.Point(92, 152);
+            this.lbDetail.Name = "lbDetail";
+            this.lbDetail.Size = new System.Drawing.Size(120, 52);
+            this.lbDetail.TabIndex = 4;
+            // 
+            // lbName
+            // 
+            this.lbName.BackColor = System.Drawing.Color.White;
+            this.lbName.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbName.Location = new System.Drawing.Point(92, 123);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(120, 19);
+            this.lbName.TabIndex = 3;
+            // 
+            // labelDetailBook
+            // 
+            this.labelDetailBook.AutoSize = true;
+            this.labelDetailBook.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDetailBook.Location = new System.Drawing.Point(8, 152);
+            this.labelDetailBook.Name = "labelDetailBook";
+            this.labelDetailBook.Size = new System.Drawing.Size(78, 19);
+            this.labelDetailBook.TabIndex = 2;
+            this.labelDetailBook.Text = "รายละเอียด :";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Prompt", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(13, 123);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(73, 19);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "ชื่อหนังสือ :";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 95);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panelDetailOrder
+            // 
+            this.panelDetailOrder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelDetailOrder.BackColor = System.Drawing.Color.Gainsboro;
+            this.panelDetailOrder.Controls.Add(this.lbEdate);
+            this.panelDetailOrder.Controls.Add(this.lbSdate);
+            this.panelDetailOrder.Controls.Add(this.label2);
+            this.panelDetailOrder.Controls.Add(this.labelSdate);
+            this.panelDetailOrder.Controls.Add(this.lbDetail);
+            this.panelDetailOrder.Controls.Add(this.lbName);
+            this.panelDetailOrder.Controls.Add(this.labelDetailBook);
+            this.panelDetailOrder.Controls.Add(this.labelName);
+            this.panelDetailOrder.Controls.Add(this.pictureBox1);
+            this.panelDetailOrder.Location = new System.Drawing.Point(694, 189);
+            this.panelDetailOrder.Name = "panelDetailOrder";
+            this.panelDetailOrder.Size = new System.Drawing.Size(227, 454);
+            this.panelDetailOrder.TabIndex = 14;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearch.Font = new System.Drawing.Font("Prompt", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(867, 37);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(54, 25);
+            this.btnSearch.TabIndex = 13;
+            this.btnSearch.Text = "ค้นหา";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSearch.Location = new System.Drawing.Point(684, 39);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(177, 20);
+            this.tbSearch.TabIndex = 12;
+            // 
+            // tabAll
+            // 
+            this.tabAll.Controls.Add(this.dgvOrdersAll);
+            this.tabAll.Font = new System.Drawing.Font("Prompt", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabAll.Location = new System.Drawing.Point(4, 22);
+            this.tabAll.Name = "tabAll";
+            this.tabAll.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAll.Size = new System.Drawing.Size(667, 460);
+            this.tabAll.TabIndex = 0;
+            this.tabAll.Text = "ทั้งหมด";
+            this.tabAll.UseVisualStyleBackColor = true;
+            // 
+            // dgvOrdersAll
+            // 
+            this.dgvOrdersAll.AllowUserToAddRows = false;
+            this.dgvOrdersAll.AllowUserToDeleteRows = false;
+            this.dgvOrdersAll.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrdersAll.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvOrdersAll.Location = new System.Drawing.Point(3, 3);
+            this.dgvOrdersAll.Name = "dgvOrdersAll";
+            this.dgvOrdersAll.ReadOnly = true;
+            this.dgvOrdersAll.Size = new System.Drawing.Size(661, 454);
+            this.dgvOrdersAll.TabIndex = 0;
+            // 
+            // btnAddBook
+            // 
+            this.btnAddBook.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddBook.Location = new System.Drawing.Point(684, 143);
+            this.btnAddBook.Name = "btnAddBook";
+            this.btnAddBook.Size = new System.Drawing.Size(75, 23);
+            this.btnAddBook.TabIndex = 18;
+            this.btnAddBook.Text = "เพิ่มหนังสือ";
+            this.btnAddBook.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabAll);
+            this.tabControl1.Location = new System.Drawing.Point(12, 164);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(675, 486);
+            this.tabControl1.TabIndex = 11;
+            // 
+            // FormBooks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(933, 662);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.labelTitleOrders);
+            this.Controls.Add(this.panelDetailOrder);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.tbSearch);
+            this.Controls.Add(this.btnAddBook);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "FormBooks";
             this.Text = "FormBooks";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelDetailOrder.ResumeLayout(false);
+            this.panelDetailOrder.PerformLayout();
+            this.tabAll.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrdersAll)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbEdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Label labelTitleOrders;
+        private System.Windows.Forms.Label lbSdate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelSdate;
+        private System.Windows.Forms.Label lbDetail;
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Label labelDetailBook;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panelDetailOrder;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.TabPage tabAll;
+        private System.Windows.Forms.DataGridView dgvOrdersAll;
+        private System.Windows.Forms.Button btnAddBook;
+        private System.Windows.Forms.TabControl tabControl1;
     }
 }
